bef5d07ec1a6f35c580075aeb033638b1557acf6
Hunter_Bann <71389997+Bann-cpu@users.noreply.github.com>
2023-04-16 14:30:59 +0200

    changes

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ gsap.to("#rect", {
   repeat: 12,
   repeatDelay: 3,
   motionPath: {
+    start: 0.25,
+    end: 0.80,
     path: "#path",
     align: "#path",
     alignOrigin: [0.5, 0.5], //aligner
     autoRotate: true
+  }
+})
+
+gsap.to(".depart-path", {
+  ease: "power1.out",
+  repeat: 5,
+  repeatDelay: 2,
+  yoyo: true,
+  duration: 5,
+  motionPath: {
+    path: "#tr-path",
+    align: "#tr-path",
+    autoRotate: true,
+    alignOrigin: [0.5, 0.5], //aligner
   }
 })
